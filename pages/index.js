1b9a62041cf4698acf635fb7365746dcc98fbfb1
smithjiyugaoka <smith@apexes.co.jp>
2024-07-20 01:27:58 +0000

    Added pages directory, updated config and package.json

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Email Sender App</h1>
+      <p>This is a basic Next.js page.</p>
+    </div>
+  );
+}
